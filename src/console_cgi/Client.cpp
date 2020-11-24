5ae74bed543fe74912cf48d38c06c8a20c2dab2c
adkevin3307 <adkevin3307@gmail.com>
2020-11-25 00:29:01 +0800

    update do_read and do_write

--- src/console_cgi/Client.cpp
+++ src/console_cgi/Client.cpp
@@ -1,6 +1,7 @@
 #include "console_cgi/Client.h"
 
 #include <boost/filesystem.hpp>
+#include <boost/algorithm/string/trim.hpp>
 
 #include "constant.h"
 #include "console_cgi/MessageHandler.h"
@@ -8,9 +9,12 @@
 using namespace std;
 
 Client::Client(boost::asio::io_context& io_context, string host, string port, string filename, int id)
-    : host(host), port(port), filename(filename), _socket(io_context), _resolver(io_context)
+    : _socket(io_context), _resolver(io_context)
 {
     this->id = to_string(id);
+    this->host = host;
+    this->port = port;
+    this->filename = filename;
 
     this->index = 0;
     this->_buffer.resize(CONSTANT::MAX_BUFFER_SIZE);
@@ -22,6 +26,8 @@ Client::Client(boost::asio::io_context& io_context, string host, string port, st
         boost::filesystem::fstream file(test_filename);
 
         while (getline(file, line)) {
+            boost::trim(line);
+
             this->commands.push_back(line);
         }
 
@@ -41,7 +47,7 @@ void Client::resolve_host()
             this->connect_host(it);
         }
         else {
-            string error_message = "Resolve error: " + error_code.message() + '\n';
+            string error_message = "Client resolve error: " + error_code.message() + '\n';
             MessageHandler::output(this->id, error_message, CONSTANT::OUTPUT_TYPE::STDERR);
         }
     });
@@ -54,7 +60,7 @@ void Client::connect_host(boost::asio::ip::tcp::resolver::iterator it)
             this->do_read();
         }
         else {
-            string error_message = "Connect error: " + error_code.message() + '\n';
+            string error_message = "Client connect error: " + error_code.message() + '\n';
             MessageHandler::output(this->id, error_message, CONSTANT::OUTPUT_TYPE::STDERR);
         }
     });
@@ -65,23 +71,21 @@ void Client::do_read()
     auto handle_buffer = boost::asio::buffer(this->_buffer, this->_buffer.size());
     this->_socket.async_read_some(handle_buffer, [this](const boost::system::error_code& error_code, size_t bytes) {
         if (!error_code) {
-            if (this->_buffer[0] == '%' && this->_buffer[1] == ' ') {
+            string s = "";
+            for (size_t i = 0; i < bytes; i++) {
+                s += this->_buffer[i];
+            }
+
+            MessageHandler::output(this->id, s, CONSTANT::OUTPUT_TYPE::STDOUT);
+
+            if (s.find('%') != string::npos) {
                 this->do_write();
             }
-            else {
-                string s;
-                for (size_t i = 0; i < bytes; i++) {
-                    s += this->_buffer[i];
-                }
-                s += '\n';
 
-                MessageHandler::output(this->id, s, CONSTANT::OUTPUT_TYPE::STDOUT);
-
-                this->do_read();
-            }
+            this->do_read();
         }
-        else {
-            string error_message = "Read error: " + error_code.message() + '\n';
+        else if (error_code != boost::asio::error::eof && error_code != boost::asio::error::operation_aborted) {
+            string error_message = "Client read error: " + error_code.message() + '\n';
             MessageHandler::output(this->id, error_message, CONSTANT::OUTPUT_TYPE::STDERR);
         }
     });
@@ -92,21 +96,28 @@ void Client::do_write()
     string command = this->commands[this->index++];
 
     if (command.empty()) {
-        MessageHandler::output(this->id, "% \n", CONSTANT::OUTPUT_TYPE::STDOUT);
+        MessageHandler::output(this->id, "\n", CONSTANT::OUTPUT_TYPE::STDOUT);
     }
     else {
         command += '\n';
 
-        auto handle_buffer = boost::asio::buffer(command, command.size());
+        auto handle_buffer = boost::asio::buffer(command, command.length());
         this->_socket.async_send(handle_buffer, [this, command](boost::system::error_code error_code, size_t bytes) {
             if (!error_code) {
-                MessageHandler::output(this->id, "% ", CONSTANT::OUTPUT_TYPE::STDOUT);
                 MessageHandler::output(this->id, command, CONSTANT::OUTPUT_TYPE::COMMAND);
 
-                this->do_read();
+                if (command == "exit\n") {
+                    this->_socket.close();
+
+                    boost::system::error_code trash;
+                    this->_socket.shutdown(boost::asio::ip::tcp::socket::shutdown_both, trash);
+                }
+                else {
+                    this->do_read();
+                }
             }
             else {
-                string error_message = "Write error: " + error_code.message() + '\n';
+                string error_message = "Client write error: " + error_code.message() + '\n';
                 MessageHandler::output(this->id, error_message, CONSTANT::OUTPUT_TYPE::STDERR);
             }
         });
